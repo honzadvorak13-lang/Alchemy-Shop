c38c200762f23446355a4102c1db14e487af3e16
Jan Dvorak <dvora261@student.vspj.cz>
2026-09-15 21:17:52 +0200

    id objects

--- src/Controller/AlchemyShopController.php
+++ src/Controller/AlchemyShopController.php
@@ -2,23 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\AlchemyProducts;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\Tests\Models\Enums\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AlchemyShopController extends AbstractController
 {
-    #[Route('/alchemy/shop', name: 'app_alchemy_shop')]
-    public function index(): Response
+    #[Route('/alchemy/shop/{id}', name: 'app_alchemy_shop')]
+    public function index(?AlchemyProducts $product): Response
     {
-        return $this->render('alchemy_shop/index.html.twig', [
-            'controller_name' => 'AlchemyShopController',
-        ]);
+        if ($product == null) {
+            return new Response("Sorry I wasn't able to find the product");
+        }
+
+        return new Response($product->getDescription());
     }
 
     #[Route('/alchemy/create', name: 'app_create_product')]
-    public function CreateProducts()
+    public function CreateProducts(EntityManagerInterface $entityManager): Response
     {
-        
+        $product = new AlchemyProducts();
+        $product->setName("Levander");
+        $product->setDescription("The violet lavender is used in many mysterious recipes....");
+        $product->setPrice(470);
+        $product->setQuantity(18);
+
+        //doctrine knows we want this product eventually stored in the database
+        // flush() executes the query
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return new Response('Saved new product with id' . $product->getId());
     }
 }
